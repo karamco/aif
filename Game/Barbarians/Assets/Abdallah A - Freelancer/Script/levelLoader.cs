72081db581bc2b474b2452c2fb13cc4fe4b88c59
abdallatTaha <abodfox2014@gmail.com>
2018-06-21 14:12:28 +0300

    add my part in Abdallah T -Freelancer

---
+++ Game/Barbarians/Assets/Abdallah A - Freelancer/Script/levelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class levelLoader : MonoBehaviour {
+
+    public GameObject LoadingScren;
+    public Slider slider;
+    public Text ProgressText;
+
+	public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        LoadingScren.SetActive(true);
+        while(!operation.isDone)
+        {
+            float Progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = Progress;
+            ProgressText.text = Progress * 100f + "%";
+            yield return null;
+        }
+    } 
+}
